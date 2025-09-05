503ac186d5b56b9a78bcbdf1d2fb318f01c16946
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-05 16:28:13 +0530

    create built_in_functions.py and math_module.py

---
+++ 04_arithmetic_&_math/in_built_math_functions.py
@@ -0,0 +1,10 @@
+
+x = 3.14
+y= 4
+z =5
+
+result = round(x) # 3
+result = abs(y) # 4
+result = pow(4,3) # 64 4^3
+result = max(x,y,z) # 5
+result = min(x,y,z) # 3.14
---
+++ 04_arithmetic_&_math/math_module.py
@@ -0,0 +1,12 @@
+
+# basic fucntions from math module
+import math
+
+x = 9
+
+print(math.pi) # value of pi
+print(math.e) # value of E constant
+
+result = math.sqrt(x) # square root of 9 = 3.0
+result = math.ceil(x) # round up to the higher number = 10
+result = math.floor(x) # round up to the lower number = 9
