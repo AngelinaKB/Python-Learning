05909d87d3b4f603c696e7eab4d129a72fd9f7da
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-05 16:15:47 +0530

    create if_else_statements folder

---
+++ 03_user_input/input.py
@@ -0,0 +1,12 @@
+
+# takes string as input
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# takes integer as input
+age = int(input("Enter your age: "))
+print("You will be", age + 1, "next year.")
+
+#takes float as input
+height = float(input("Enter your height in meters: "))
+print("Your height is", height, "meters.")
