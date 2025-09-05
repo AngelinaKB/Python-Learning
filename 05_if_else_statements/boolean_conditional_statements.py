c304b5ad5655aeca443588d20f274c92315486da
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-05 17:10:56 +0530

    create boolean_conditional_statements.py

---
+++ 05_if_else_statements/boolean_conditional_statements.py
@@ -0,0 +1,6 @@
+for_sale= True
+
+if for_sale:
+    print("This item is for sale! :D")
+else: 
+    print("This item is not for sale! :<")
--- 05_if_else_statements/if_else.py
+++ 05_if_else_statements/if_else.py
@@ -1,8 +1,19 @@
+# if = you do something if condition is True
+# else = you dont something else
+
 x = 3
 
 if x > 5:
-    print("x is greater than 5")
+    print("x is greater than 5") 
 elif x == 5:
     print("x is equal to 5")
 else:
     print("x is less than 5")
+
+
+response =  input("Would you like food? (Y/N): ").upper()
+if response == "Y":
+    print("Here is some sushi! 🍣 (๑ᵔ⤙ᵔ๑ 🥢)")
+else:
+    print("\nOkay! Maybe later?")
+    
