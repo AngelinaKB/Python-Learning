011e7701e781891f3a9d65d3bdb8fa75559aee2c
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-10 22:48:48 +0530

    create 09_format_specifiers and create file

---
+++ 09_format_specifiers/format.py
@@ -0,0 +1,13 @@
+# fprmat specifiers = {value:flags} format a value based on what
+#                              flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number)f = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = place sign to leftmost position
+# :  = insert a space before positive numbers
+# :, = comma separator
