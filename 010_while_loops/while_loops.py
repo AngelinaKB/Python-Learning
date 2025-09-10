8829253fa28267d322e0f912b38cd1647bc4065b
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-10 23:22:06 +0530

    updated while_loops.py

--- 010_while_loops/while_loops.py
+++ 010_while_loops/while_loops.py
@@ -1,5 +1,6 @@
 # while loop = execute some code WHILE some condition remains True
 
+# Example 1
 name = input("Enter your name: ")
 
 while name=="":
@@ -7,3 +8,20 @@ while name=="":
     name = input("Enter your name: ")
 
 print(f"Your name is {name.capitalize()}.")
+
+# Example 2
+food = input("Enter a food you like (press q to quit): ")
+while not food== "q":
+    print(f"You like {food}!")
+    food = input("Enter a food you like (press q to quit): ")
+
+print("Bye! ^-^")
+
+# Example 3
+num= int(input("Enter a number between 1 - 10: "))
+
+while num<1 or num>10: 
+    print(f"{num} is invalid!")
+    num= int(input("Enter a number between 1 - 10: "))
+
+print(f"Your number is {num}")
