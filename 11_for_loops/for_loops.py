8f66034aeeb1fb3cd40adc3a4815d8fb80f08ba7
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-15 00:03:02 +0530

    update for_loops.py

--- 11_for_loops/for_loops.py
+++ 11_for_loops/for_loops.py
@@ -7,3 +7,26 @@ for x in range(1,11):
 # print reversed numbering 10-1
 for x in reversed(range(1,11)):
     print(x,end=" ")
+
+# print numbers 1-10 with a step of 2
+for x in range(1,11,2):
+    print(x,end=" ")
+
+# loop over string
+credit_number = "1234-5678-9102-3456"
+for x in credit_number:
+    print(x, end="")
+
+# continue keyword allows you to pass over certain values
+for x in range(1,21):
+  if x == 13:
+    continue
+  else:
+    print(x, end=" ")
+
+# break keyword allows you to exit out when certain condition is met
+for x in range(1,21):
+  if x == 13:
+    break
+  else:
+    print(x, end=" ")
