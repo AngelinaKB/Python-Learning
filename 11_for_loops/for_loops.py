02178682be3f61e18f0869d3899079d19fe09790
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-14 23:58:36 +0530

    create for_loop folder and file

---
+++ 11_for_loops/for_loops.py
@@ -0,0 +1,9 @@
+# for = execute block of code in a fixed number of times. 
+
+# print numbers 1-10
+for x in range(1,11):
+    print(x, end=" ")
+
+# print reversed numbering 10-1
+for x in reversed(range(1,11)):
+    print(x,end=" ")
