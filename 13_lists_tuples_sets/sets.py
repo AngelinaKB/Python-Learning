c8ec276a12a1df86479e2cbc03c9a4143d068cf8
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-16 22:22:54 +0530

    create sets.py

---
+++ 13_lists_tuples_sets/sets.py
@@ -0,0 +1,22 @@
+# cant index a set
+
+fruits =  {"apple", "banana", "orange", "pear"}
+print(fruits)
+
+# find length of set
+print(len(fruits))
+
+# find if an element in list,  gives boolean answer 
+print("apple" in fruits)
+
+# add elements
+fruits.add("pineapple")
+
+# remove elements
+fruits.remove("apple")
+
+# remove whatever element is first, randomized 
+fruits.pop()
+
+# clear 
+fruits.clear()
