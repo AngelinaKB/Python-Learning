bfb68091dca8c76cb4faf40f588b49b3c2de7fd7
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-16 22:26:27 +0530

    create tuples.py

---
+++ 13_lists_tuples_sets/tuples.py
@@ -0,0 +1,18 @@
+
+fruits =  ("apple", "banana", "orange", "pear","coconuts","coconuts")
+print(fruits)
+
+# find length of tuple
+print(len(fruits))
+
+# find if an element in list,  gives boolean answer 
+print("apple" in fruits)
+
+# find index of element
+fruits.index("apple")
+
+# count of elements
+fruits.count("coconuts")
+
+for x in fruits:
+    print(x)
