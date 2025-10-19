d2502fc8e8c401b4f2f3d752b63026a2cc8073b4
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-19 23:06:25 +0530

    Update 2D_list.py

--- 14_2D_collections/2D_list.py
+++ 14_2D_collections/2D_list.py
@@ -1,7 +1,17 @@
-fruits = ["apple", "orange", "banana", "coconut"]
 vegetables = ["celery", "carrots", "potatoes"]
-meats = ["chicken", "fish", "turkey"]
+meats =      ["chicken", "fish", "turkey"]
 
-groceries = [fruits, vegetables, meats]
+groceries = [["apple", "orange", "banana", "coconut"], vegetables, meats]
 
-print(groceries)
+print(groceries[0]) # first index => entire list
+print(groceries[0][3]) # first element of the first index list
+
+# Iterate over elements of 2d list
+
+for collection in groceries:
+  print(collection)
+  
+# to iterate over elements inside each list
+for collection in groceries:
+  for food in collection:
+    print(food)
