d0b9a57fede102530b606c970986e726275ca0ec
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-20 23:34:59 +0530

    Update random.py

--- 16_random_module/random.py
+++ 16_random_module/random.py
@@ -7,8 +7,13 @@ options = ("rock", "paper", "scissors")
 cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 number = random.randint(low, high) # random number between given range 
-num = random.random() # random floating point number 
-options = random.choice(options) # random choice 
-shuffle =  random.shuffle(cards) # shuffle deck randomly 
+print(number)
 
+num = random.random() # random floating point number 
+print(num)
+
+options = random.choice(options) # random choice 
+print(options)
+
+shuffle =  random.shuffle(cards) # shuffle deck randomly 
 print(cards)
