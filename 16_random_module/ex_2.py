b93e8fbc3ebaa87c41a52cfd3e102ac8d0797fe2
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-20 23:56:12 +0530

    Update ex_2.py

--- 16_random_module/ex_2.py
+++ 16_random_module/ex_2.py
@@ -1,1 +1,18 @@
+import random
 
+options = ("Rock", "Paper", "Scissors")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player =  input("Enter a choice (Rock, Paper, Scissors): ")
+
+
+
+print(f"Player: {player.capitalize()}")
+print(f"Computer : {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "Rock" and computer == "Scissors":
+    print("You win!")
