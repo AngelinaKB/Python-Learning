4d0569f0f80ef92220be2c643213d3cccc67bcd9
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-23 21:48:19 +0530

    Update functions.py

--- 16_functions/functions.py
+++ 16_functions/functions.py
@@ -1,1 +1,15 @@
+# function - A block of reusable code
+#            place () after the function name to invoke it
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print(f"Happy birthday to {name}!")
+    print()
+    
+happy_birthday("Ryan",20)
+happy_birthday("Steve",30)
+happy_birthday("Joe",40)
+
+
 
