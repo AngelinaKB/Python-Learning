dfe7c576b15921c48684225b673e27990dd68ce6
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-25 22:32:56 +0530

    Update default.py

--- 17_functions/default.py
+++ 17_functions/default.py
@@ -2,6 +2,7 @@
 #                      default is used when that arguement is omitted
 #                      make your functions more flexible, reduces # of arguements 
 #                      1. postional 2. DEFAULT 3. keyword 4. arbitrary
+import time
 
 def net_price(list_price, discount = 0, sales_tax = 0.05):
     return list_price *  (1 - discount) * (1 + sales_tax)
@@ -9,3 +10,16 @@ def net_price(list_price, discount = 0, sales_tax = 0.05):
 print(net_price(500))
 print(net_price(500, 0.1)) # 10% discount
 print(net_price(500, 0.1, 0)) # no sales tax
+
+
+# Simple Time program 
+
+def count(end, start= 0,): # non default arguments should be before default arguments 
+    for x in range(start, end + 1):
+        print(x)
+        time.sleep(1)
+        
+    print("done")
+
+# print(count(5))
+print(count(30,15))
