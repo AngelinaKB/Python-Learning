342638b3e815b2fb7599fd4bbb4b747460577e9f
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-25 22:46:51 +0530

    Update keyword.py

--- 17_functions/keyword.py
+++ 17_functions/keyword.py
@@ -10,8 +10,18 @@ print(hello("Hello", "Mr.", "Spongebob", "Squarepants")) # positional arguements
 print(hello("Hello", "Spongebob", "Squarepants", "Mr.")) 
 print(hello("Hello", first = "Spongebob", last = "Squarepants", title ="Mr.")) 
 
+# end arguement 
 for x in range(1,11):
     print(x, end =" ")
-    
+
+# sep arguement
 print()
 print("1", "2", "3", "4", "5", sep = "-")
+
+# function to generate a phone number
+
+def generate_phone_no(country_code, area_code, first_digits, last_digits):
+    return f"{country_code}-{area_code}-{first_digits}-{last_digits}"
+
+phone_num = generate_phone_no(country_code= 1, area_code= 123, first_digits= 456, last_digits= 7890)
+print(f"Your phone number is {phone_num}")
