7889f81623986a42c679dcff5cdc13b591f11dae
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-26 20:44:01 +0530

    Create keyword_arguements.py

---
+++ 17_functions/keyword_arguements.py
@@ -0,0 +1,40 @@
+#   **kwargs = keyword arguements 
+
+
+def print_address(**kwargs):
+    return type(kwargs)
+
+print(print_address(street ="123 Fake Street",
+                    city= "Detroit", 
+                    state= "MI", 
+                    zip="54321"))
+
+# iterate over kwargs
+def print_address_iterate(**kwargs):
+    for key,value in kwargs.items():
+        print(key, ":", value)
+
+print(print_address_iterate(street ="123 Fake Street",
+                    city= "Detroit", 
+                    state= "MI", 
+                    zip="54321"))
+
+# shipping label program
+
+def shipping_label(*args, **kwargs):
+    print("To,")
+    for arg in args:
+        print(arg, end = " ")
+    
+    print()
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} { kwargs.get('apt')}")
+    else:
+        print(f"{kwargs.get('street')}")
+    print(f"{kwargs.get('city')} {kwargs.get('state')}, {kwargs.get('zip')}")
+
+print(shipping_label("Dr.", "Spongebob", "Squarepants", "III", 
+                    street ="123 Fake Street",
+                    city= "Detroit", 
+                    state= "MI", 
+                    zip="54321"))
