7a8ac0b86b88995565057491dbf90e46bc203c09
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-26 20:30:53 +0530

    Update arbitrary_arguements.py

--- 17_functions/arbitrary_arguements.py
+++ 17_functions/arbitrary_arguements.py
@@ -1,1 +1,28 @@
+# *args = allows you to pass multiple non-key arguements  , packs into tuple
+# **kwargs = allows you to pass multiple keyword-arguements , packs into dictionary
+#            * unpacking operator
+#               
 
+def add(a,b):
+    return a+b
+
+print(add(1,2)) # doesnt accept multiple arguements
+
+def multiple_args(*args):
+    return type(args)
+print(multiple_args(1,2,3)) # accepts multiple arguements 
+
+def multiple_args_2(*nums): # iterate over args
+    total = 0
+    for num in nums:
+        total += num
+    return total
+print(multiple_args_2(1,2,3)) 
+
+# function to display name
+
+def display_name(*args):
+    for arg in args:
+        print(arg, end =" ")
+        
+display_name("Dr.", "Spongebob", "Harold", "Squarepants", "III") 
